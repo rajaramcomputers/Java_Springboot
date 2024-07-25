802440589c6946dac7d8f248e08bb209d1635d98
unknown <jagdeeshbhavani22@gmail.com>
2024-07-26 00:59:16 +0530

    till lec-319

---
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
@@ -0,0 +1,29 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Course;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+    public Instructor findInstructorById(int id);
+    void deleteInstructorById(int id);
+
+    public InstructorDetail findInstructorDetailId(int id);
+
+    void deleteInstructorDetail(int id);
+
+    List<Course> findCoursesByInstructorId(int id);
+    Instructor findCoursesByInstructorIdJOINFetch(int id);
+    void updateInstructorById(Instructor theInstructor);
+
+    Course findCourseById(int id);
+    void updateCourseById(Course theCourse);
+
+    void deleteInstructorByIdOfCourses(int id);
+    void deleteCourseById(int id);
+    void save(Course theCourse);
+    Course findCourseAndReviewsByCourseId(int id);
+}
